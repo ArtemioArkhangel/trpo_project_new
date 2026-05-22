d442c8c8856ab2da10eaf0799341f2c00b8f7438
Artem Arhangelskij <migastol@mail.ru>
2026-05-23 01:21:32 +0700

    add customing and generation functions

--- pwgen/src/pwgenfunc.c
+++ pwgen/src/pwgenfunc.c
@@ -1,4 +1,40 @@
 #include "pwgenfunc.h"
+void custom_password(char *char_kit, int useLowers, int useUppers, int useDigits, int useSigns, int useSpecialSigns)
+{
+    char Lowers[] = "abcdefghijklmnopqrstuvwxyz";
+    char Uppers[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char Digits[] = "0123456789";
+    char Signs[] = "!@#$%&*()-_=+.,;:";
+    char all_Signs[] = "!@#$%&*()-_=+.,;:/?'<=>[]^|{}`~";
+    if (useLowers == 1)
+        strcat(char_kit, Lowers);
+    if (useUppers == 1)
+        strcat(char_kit, Uppers);
+    if (useDigits == 1)
+        strcat(char_kit, Digits);
+    if (useSpecialSigns == 1)
+        strcat(char_kit, all_Signs);
+    else if (useSigns == 1)
+        strcat(char_kit, Signs);
+    size_t char_kit_size = strlen(char_kit);
+    if (char_kit_size == 0)
+    {
+        printf("Ошибка, Вы не выбрали ни одного типа символов\n");
+        return;
+    }
+}
+void GeneratePassword(char *str, int char_num, char *buffer, int pw_num)
+{
+    for (int i = 0; i < pw_num; i++)
+    {
+        for (int j = 0; j < char_num; j++)
+        {
+            str[j] = buffer[rand() % strlen(buffer)];
+        }
+        str[char_num] = '\0';
+        printf("%s\n", str);
+    }
+}
 void check_char_num(int res, int ch, int *char_num)
 {
     printf("Сколько символов должен иметь пароль?(от 4 до 60)\n");
